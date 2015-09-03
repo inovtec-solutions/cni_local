3215b8971b148634e27dfd3053a92210f76aad39
Bahar Ali <engr.baharali@gmail.com>
2015-09-04 01:18:37 +0500

    changes for import wizard

--- cni/wizard/cni_wizard_import_project_data.py
+++ cni/wizard/cni_wizard_import_project_data.py
@@ -22,20 +22,39 @@ class cni_import_project_data(osv.osv_memory):
         worksheet = workbook.sheet_by_name('project_data')
         rows = worksheet.nrows - 1
         cells = worksheet.ncols - 1
-        row = 1
-        
+        row = 7
+            
         while row <= rows:
-            _logger.info("Excel Rows : %r", worksheet.cell_value(row, 3))
-            print "rows: ", rows
-            row += 1
-            continue;
-            project_exist = self.pool.get('project.project').search(cr, uid, [('network','=',worksheet.cell_value(row, 4)),('transaction_no','=',worksheet.cell_value(row, 1))])
+            
+            if row >= 137:
+                break 
+            
+            transaction_no = str(worksheet.cell_value(row, 1))
+            project_id_excel = str(worksheet.cell_value(row, 2))
+             
+            _logger.info("Transaction No.: %r, Project ID: %r", transaction_no, project_id_excel)
+            
+            try:
+                float(transaction_no) # for int, long and float
+            except ValueError:
+                row += 1    
+                continue
+            
+            _logger.info("___________________________%r", transaction_no)
             
             if worksheet.cell_value(row, 31) == 'P-A':
+                _logger.info("_______________P-A____________%r", worksheet.cell_value(row, 31))
+            
+            
+                project_exist = self.pool.get('project.project').search(cr, uid, [('project_id','=',project_id_excel)])
                 if project_exist:
                     project_id = project_exist[0]
+                    _logger.info("_______________if project____________%r", worksheet.cell_value(row, 31))
+            
                 else:
                     project_id = self.pool.get('project.project').create(cr, uid, {
+                        'name': worksheet.cell_value(row, 9),
+                        'project_types': 'Pre-Assembly',
                         'project_id': worksheet.cell_value(row, 2),
                         'network': worksheet.cell_value(row, 4),
                         'actv_desc': worksheet.cell_value(row, 6),
@@ -44,23 +63,36 @@ class cni_import_project_data(osv.osv_memory):
 
 
                 material_exist = self.pool.get('project.material').search(cr, uid, [('name','=',project_id),('transaction_no','=',worksheet.cell_value(row, 1))])
+                
+                shiping_date = str(worksheet.cell_value(row,46))
+                if shiping_date.strip() == "":
+                    shiping_date = None
+                else:
+                    shiping_date = shiping_date.replace(".", "/")
+                
+                material_req_date = str(worksheet.cell_value(row, 16))
+                if material_req_date.strip() == "":
+                    material_req_date = None
+                else:
+                    material_req_date = material_req_date.replace(".", "/")
+                    
                 if material_exist:
                     material_id = material_exist[0]
                     self.pool.get('project.material').write(cr, uid, material_id, {
-                        'material_req_date': worksheet.cell_value(row, 16),
+                        'material_req_date': material_req_date,
                         'mat_desc': worksheet.cell_value(row, 22),
                         'req_quantiity': worksheet.cell_value(row,37),
-                        'shiping_date': worksheet.cell_value(row,46),
+                        'shiping_date': shiping_date,
                         'delivery_pa': worksheet.cell_value(row,63),
                         'pa_gi_doc': worksheet.cell_value(row,67)}, context=context)
                 else:
-                    material_id = self.pool.get('project.material').create(cr, uid, {
-                        'name': material_id,
+                    self.pool.get('project.material').create(cr, uid, {
+                        'name': project_id,
                         'transaction_no': worksheet.cell_value(row, 1),
-                        'material_req_date': worksheet.cell_value(row, 16),
+                        'material_req_date': material_req_date,
                         'mat_desc': worksheet.cell_value(row, 22),
                         'req_quantiity': worksheet.cell_value(row,37),
-                        'shiping_date': worksheet.cell_value(row,46),
+                        'shiping_date': shiping_date,
                         'delivery_pa': worksheet.cell_value(row,63),
                         'pa_gi_doc': worksheet.cell_value(row,67)}, context=context)            
 
