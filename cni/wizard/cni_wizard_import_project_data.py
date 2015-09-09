290592753cdc2dd9a8d8f0ed44f6c80d69325649
Shahid Islam <wfrisson@gmail.com>
2015-09-09 10:18:54 +0500

    Changes done for cni

--- cni/wizard/cni_wizard_import_project_data.py
+++ cni/wizard/cni_wizard_import_project_data.py
@@ -25,42 +25,6 @@ class cni_import_project_data(osv.osv_memory):
         row = 7
             
         while row <= rows:
-<<<<<<< HEAD
-            _logger.info("Excel Rows : %r", worksheet.cell_value(row, 3))
-            print "rows: ", rows
-            row += 1
-            continue;
-            project_exist = self.pool.get('project.project').search(cr, uid, [('network','=',worksheet.cell_value(row, 3)),('transaction_no','=',worksheet.cell_value(row, 3))])
-            
-            if project_exist:
-                self.pool.get('project.project').write(cr, uid, project_exist[0], {
-                    'network': worksheet.cell_value(row, 1),
-                    'priority': worksheet.cell_value(row, 1),
-                    'primevera_id': worksheet.cell_value(row, 2),
-                    'actv_desc': worksheet.cell_value(row, 3),
-                    'wbs': worksheet.cell_value(row, 4),
-                    'delivery_pa': worksheet.cell_value(row,5),
-                    'status': worksheet.cell_value(row,6),
-                    'site_code': worksheet.cell_value(row,7),
-                     }, context=context)
-            else:
-                self.pool.get('project.project').create(cr, uid, {
-                    'transaction_no': worksheet.cell_value(row, 1),
-                    'network': worksheet.cell_value(row, 2),
-                    'priority': worksheet.cell_value(row, 3),
-                    'primevera_id': worksheet.cell_value(row, 4),
-                    'actv_desc': worksheet.cell_value(row, 5),
-                    'wbs': worksheet.cell_value(row, 6),
-                    'delivery_pa': worksheet.cell_value(row,7),
-                    'status': worksheet.cell_value(row,6),
-                    'site_code': worksheet.cell_value(row,8),
-                    }, context=context)
-            
-=======
-            
-            if row >= 137:
-                break 
-            
             transaction_no = str(worksheet.cell_value(row, 1))
             project_id_excel = str(worksheet.cell_value(row, 2))
              
@@ -128,7 +92,6 @@ class cni_import_project_data(osv.osv_memory):
                         'delivery_pa': worksheet.cell_value(row,63),
                         'pa_gi_doc': worksheet.cell_value(row,67)}, context=context)            
 
->>>>>>> 3215b8971b148634e27dfd3053a92210f76aad39
             row += 1
         return {}
 
